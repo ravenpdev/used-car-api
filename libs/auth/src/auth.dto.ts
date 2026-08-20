2a0aa3281da26a3b7be64ef4865d20dc9fb7df3a
Raven Paragas <ravenp.dev@gmail.com>
2026-08-20 08:58:30 +0800

    test: add more test in auth.e2e-spec.ts
    update SignupDto password must contain atleast 1 uppercase and 1 symbol

--- libs/auth/src/auth.dto.ts
+++ libs/auth/src/auth.dto.ts
@@ -3,8 +3,26 @@ import { z } from 'zod';
 
 const signupSchema = z
   .object({
-    email: z.email(),
-    password: z.string().min(8),
+    email: z.email({
+      error: 'Invalid email',
+    }),
+    password: z
+      .string()
+      .min(8)
+      .superRefine((val, ctx) => {
+        if (!/[A-Z]/.test(val)) {
+          ctx.addIssue({
+            code: 'custom',
+            message: 'Password must contain at least 1 uppercase letter',
+          });
+        }
+        if (!/[^A-Za-z0-9]/.test(val)) {
+          ctx.addIssue({
+            code: 'custom',
+            message: 'Password must contain at least 1 symbol',
+          });
+        }
+      }),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -3,11 +3,77 @@ import { SigninDto, SignupDto } from '@app/auth/auth.dto';
 import { app } from './setup';
 
 describe('Authentication (e2e)', () => {
+  it('should return 400 when email is invalid', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: 'invalid-email',
+        password: 'P@ssw0rd',
+        confirmPassword: 'P@ssw0rd',
+      })
+      .expect(400);
+
+    expect(response.body).toMatchObject({
+      statusCode: 400,
+      message: 'Validation failed',
+      errors: [
+        {
+          path: ['email'],
+          message: 'Invalid email',
+        },
+      ],
+    });
+  });
+
+  it('should return 400 when password is less than 8 characters', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'P@ssw0',
+        confirmPassword: 'P@ssw0',
+      })
+      .expect(400);
+
+    expect(response.body).toMatchObject({
+      statusCode: 400,
+      message: 'Validation failed',
+      errors: [
+        {
+          path: ['password'],
+          message: expect.stringContaining('small'),
+        },
+      ],
+    });
+  });
+
+  it('should return 400 when password and confirmPassword is not match', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'P@ssw0rd',
+        confirmPassword: 'P@ssw0rd1',
+      })
+      .expect(400);
+
+    expect(response.body).toMatchObject({
+      statusCode: 400,
+      message: 'Validation failed',
+      errors: [
+        {
+          path: ['confirmPassword'],
+          message: 'Password did not match',
+        },
+      ],
+    });
+  });
+
   it('handles a signup request', async () => {
     const dto = {
       email: 'demo1@test.com',
-      password: 'password',
-      confirmPassword: 'password',
+      password: 'P@ssw0rd',
+      confirmPassword: 'P@ssw0rd',
     };
     const response = await request(app.getHttpServer())
       .post('/auth/signup')
@@ -22,8 +88,8 @@ describe('Authentication (e2e)', () => {
   it('should return a 409 ConflictException', async () => {
     const dto: SignupDto = {
       email: 'demo@test.com',
-      password: 'password',
-      confirmPassword: 'password',
+      password: 'P@ssw0rd',
+      confirmPassword: 'P@ssw0rd',
     };
 
     await request(app.getHttpServer())
@@ -40,13 +106,13 @@ describe('Authentication (e2e)', () => {
   it('handles a signin request', async () => {
     const signupDto: SignupDto = {
       email: 'demo@test.com',
-      password: 'password',
-      confirmPassword: 'password',
+      password: 'P@ssw0rd',
+      confirmPassword: 'P@ssw0rd',
     };
 
     const signinDto: SigninDto = {
       email: 'demo@test.com',
-      password: 'password',
+      password: 'P@ssw0rd',
     };
 
     await request(app.getHttpServer())
@@ -67,12 +133,34 @@ describe('Authentication (e2e)', () => {
   it('should return 401 UnauthorizedException', async () => {
     const siginDto: SigninDto = {
       email: 'test@test.com',
-      password: 'password',
+      password: 'P@ssw0rd',
     };
 
     await request(app.getHttpServer())
       .post('/auth/signin')
       .send(siginDto)
       .expect(401);
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const signupDto: SignupDto = {
+      email: 'demo@test.com',
+      password: 'P@ssw0rd',
+      confirmPassword: 'P@ssw0rd',
+    };
+
+    // agent keeps the cookie/session between request
+    const agent = request.agent(app.getHttpServer());
+
+    await agent.post('/auth/signup').send(signupDto).expect(201);
+
+    const response = await agent.get('/auth/whoami').expect(200);
+    const { email } = response.body;
+
+    expect(email).toEqual(signupDto.email);
+  });
+
+  it('should return a 403 if not authenticated and tried to access whoami', async () => {
+    await request(app.getHttpServer()).get('/auth/whoami').expect(403);
   });
 });
